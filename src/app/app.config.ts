b7969a47b0847145327d8468c43b0d17b9063527
Derick Fynn <dcfynn@icloud.com>
2025-02-15 18:38:11 +0200

    feat(app): Project Setup WIP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideCharts(withDefaultRegisterables())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideCharts(withDefaultRegisterables()),
+    provideAnimations(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'resume',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+    children: [
+      // {
+      //   path: 'about',
+      //   // component: AboutComponent
+      // },
+      // {
+      //   path: 'resume',
+      //   component: ResumeComponent
+      // },
+      // {
+      //   path: 'portfolio',
+      //   component: PortfolioComponent
+      // },
+      // {
+      //   path: 'contact',
+      //   component: ContactComponent
+      // },
+    ],
+  },
+  {
+    path: '',
+    component: AppComponent,
+  },
+];
