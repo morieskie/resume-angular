e91c11636da6814525c978d257ae7932c2907978
Derick Fynn <dcfynn@icloud.com>
2025-02-15 19:29:38 +0200

    feat(testing): Added mock component and route for about

---
+++ src/app/mocks/mock-about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-about',
+  template: `<div>About Component</div>`,
+})
+export class MockAboutComponent {}
--- src/app/mocks/mock-routes.ts
+++ src/app/mocks/mock-routes.ts
@@ -1,9 +1,16 @@
+import { MockAboutComponent } from './mock-about.component';
 import { MockHomeComponent } from './mock-home.component';
 
 export const mockRoutes = [
   {
-    path: 'resume',
+    path: 'home',
     component: MockHomeComponent,
+    children: [
+      {
+        path: "about",
+        component: MockAboutComponent
+      }
+    ]
   },
   {
     path: '',
