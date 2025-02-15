6178d6473127979fed060e64b3f11238df5b6429
Derick Fynn <dcfynn@icloud.com>
2025-02-15 18:39:43 +0200

    feat(testing): Added mock routes and mock component for homeComponent

---
+++ src/app/mocks/mock-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: `<div>Home Component</div>`,
+})
+export class MockHomeComponent {}
---
+++ src/app/mocks/mock-routes.ts
@@ -0,0 +1,12 @@
+import { MockHomeComponent } from './mock-home.component';
+
+export const mockRoutes = [
+  {
+    path: 'resume',
+    component: MockHomeComponent,
+  },
+  {
+    path: '',
+    component: MockHomeComponent,
+  },
+];
