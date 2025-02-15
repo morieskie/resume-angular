ceb0dfa36cff20f2908dccef12b17262418c8c02
Derick Fynn <dcfynn@icloud.com>
2025-02-15 20:18:22 +0200

    feat(AboutComponent): Added and initialised about component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SectionComponent } from "../shared/components/section/section.component";
+
+@Component({
+  selector: 'app-about',
+  imports: [SectionComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
