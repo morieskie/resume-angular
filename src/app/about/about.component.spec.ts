4e2648be18b9f5cc3c7ffefb5a5a4649cf81672f
Derick Fynn <dcfynn@icloud.com>
2025-02-16 22:20:00 +0200

    feat(AboutComponent): Impelemented Logic, unit tests, Ui, styling and Animations

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,6 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { SectionComponent } from '../shared/components/section/section.component';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+import { ProfileInterface } from '../shared/interfaces/profile.interface';
+import { ProfileService } from '../shared/service/profile.service';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,9 +18,23 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutComponent]
+      providers: [
+        provideNoopAnimations(),
+        {
+          provide: ProfileService,
+          useValue: {
+            getDetails: () => of({ name: 'Test' } as ProfileInterface),
+          },
+        },
+      ],
     })
-    .compileComponents();
+      .overrideComponent(SectionComponent, {
+        set: {
+          selector: 'app-section',
+          template: '<p>Mock Section Component</p>',
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
@@ -20,4 +44,11 @@ describe('AboutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should retrieve user profile from backend and update model value', fakeAsync(() => {
+    tick();
+    component.ngOnInit();
+    tick();
+    expect((component.model as any).name).toEqual('Test');
+  }));
 });
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,136 @@
-import { Component } from '@angular/core';
-import { SectionComponent } from "../shared/components/section/section.component";
+import { Component, DestroyRef, inject } from '@angular/core';
+import { SectionComponent } from '../shared/components/section/section.component';
+import {
+  animate,
+  query,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { NgOptimizedImage } from '@angular/common';
+import { ProfileService } from '../shared/service/profile.service';
+import { take } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ProfileInterface } from '../shared/interfaces/profile.interface';
 
 @Component({
   selector: 'app-about',
-  imports: [SectionComponent],
+  imports: [SectionComponent, NgOptimizedImage],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
+  animations: [
+    trigger('fadeInLeft', [
+      state(
+        'off',
+        style({
+          transform: 'translateX(-100%)',
+          visibility: 'hidden',
+        })
+      ),
+      state(
+        'on',
+        style({
+          transform: 'translateX(0)',
+          visibility: 'visible',
+        })
+      ),
+      transition('void => on', [
+        style({
+          transform: 'translateX(-100%)',
+          visibility: 'hidden',
+        }),
+        animate(
+          '350ms cubic-bezier(0.35, 0, 0.25, 1)',
+          style({
+            transform: 'translateX(0)',
+            visibility: 'visible',
+          })
+        ),
+      ]),
+    ]),
+    trigger('fadeInRight', [
+      state(
+        'off',
+        style({
+          transform: 'translateX(400px)',
+          visibility: 'hidden',
+        })
+      ),
+      state(
+        'on',
+        style({
+          transform: 'translateX(0)',
+          visibility: 'visible',
+        })
+      ),
+      transition('void => on', [
+        query('[data-animation-effect="fadeInRight"]', [
+          style({
+            transform: 'translateX(100%)',
+            visibility: 'hidden',
+          }),
+          stagger(200, [
+            animate(
+              '350ms cubic-bezier(0.35, 0, 0.25, 1)',
+              style({
+                transform: 'translateX(0)',
+                visibility: 'visible',
+              })
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+    trigger('fadeInUp', [
+      state(
+        'off',
+        style({
+          transform: 'translateY(100%)',
+        })
+      ),
+      state(
+        'on',
+        style({
+          transform: 'translateY(0)',
+        })
+      ),
+      transition('void => on', [
+        style({
+          transform: 'translateY(200%)',
+        }),
+        animate(
+          '1500ms 200ms ease-in',
+          style({
+            transform: 'translateY(0)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class AboutComponent {
+  fadeInRight = 'off';
+  fadeInLeft = 'off';
+  fadeInUp = 'off';
+  model: ProfileInterface | undefined;
 
+  destroyRef: DestroyRef = inject(DestroyRef);
+
+  constructor(private service: ProfileService) {}
+
+  ngOnInit(): void {
+    this.fadeInLeft = 'on';
+    this.fadeInRight = 'on';
+    this.fadeInUp = 'on';
+    this.service
+      .getDetails()
+      .pipe(take(1), takeUntilDestroyed(this.destroyRef))
+      .subscribe((profile) => {
+        setTimeout(() => {
+          this.model = profile;
+        });
+      });
+  }
 }
