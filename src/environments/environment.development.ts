11592025a106ceb894a9bf13a2c99ada425e6e16
Derick Fynn <dcfynn@icloud.com>
2025-02-17 01:43:39 +0200

    feat(environemnt): Updated env variables added timezone & dateFormat

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
   baseApiUrl: 'http://localhost:4200',
   endpoints: {
-    profile: 'assets/data/about.json',
+    profile: 'assets/data/profile.json',
   },
+  timezone: 'Africa/Johannesburg',
+  dateFormat: 'dd/MM/yyyy',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     baseApiUrl: 'https://morieskie.github.io/resume-angular',
     endpoints: {
-      profile: 'assets/data/about',
+      profile: 'assets/data/profile.json',
     },
+    timezone: 'Africa/Johannesburg',
+    dateFormat: 'dd/MM/yyyy',
 };
