d274f034cc3d8436d5e63d822beb8ed42906ce4e
Derick Fynn <dcfynn@icloud.com>
2025-02-23 00:43:38 +0200

    feat(enviroment): Removed extra trailling comma

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    baseApiUrl: 'https://morieskie.github.io/resume-angular',
-    baseHref: '/resume-angular',
-    endpoints: {
-      profile: 'assets/data/profile.json',
-      education: 'assets/data/education.json',
-      experience: 'assets/data/experience.json',
-      testimonial: 'assets/data/testimonials.json',,
-    technology: `assets/data/technologies.json`
-    },
-    timezone: 'Africa/Johannesburg',
-    dateFormat: 'dd/MM/yyyy',
+  baseApiUrl: 'https://morieskie.github.io/resume-angular',
+  baseHref: '/resume-angular',
+  endpoints: {
+    profile: 'assets/data/profile.json',
+    education: 'assets/data/education.json',
+    experience: 'assets/data/experience.json',
+    testimonial: 'assets/data/testimonials.json',
+    technology: `assets/data/technologies.json`,
+  },
+  timezone: 'Africa/Johannesburg',
+  dateFormat: 'dd/MM/yyyy',
 };
