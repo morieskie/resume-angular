d2abf3277c3c0e98109391cbc189040ca8da443a
Derick Fynn <dcfynn@icloud.com>
2025-02-23 00:23:43 +0200

    feat(environment): Updated endpoint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,10 @@ export const environment = {
   baseHref: '',
   endpoints: {
     profile: 'assets/data/profile.json',
+    education: 'assets/data/education.json',
+    experience: 'assets/data/experience.json',
+    testimonial: 'assets/data/testimonials.json',
+    technology: `assets/data/technologies.json`
   },
   timezone: 'Africa/Johannesburg',
   dateFormat: 'dd/MM/yyyy',
