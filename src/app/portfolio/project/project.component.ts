0b56750c2e4958a08965c284213f7d582d7957c2
Derick Fynn <dcfynn@icloud.com>
2025-02-24 03:57:44 +0200

    Feat(ProjectComponent): Added styling, markup and implemented detail view of project ad impl. carousel for project images. Also added ability to navigate accross project by clicking next and previous buttons

--- src/app/portfolio/project/project.component.ts
+++ src/app/portfolio/project/project.component.ts
@@ -1,18 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-import { SectionComponent } from '../../shared/components/section/section.component';
-import { ActivatedRoute } from '@angular/router';
-import { map, take } from 'rxjs';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  Router,
+  RouterLink,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { forkJoin, map, take } from 'rxjs';
+import { ProjectInterface } from '../../shared/interfaces/project.interface';
+import { DatePipe, NgFor } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { selectProject } from '../../../store/state/resume.selectors';
+import {
+  CarouselComponent,
+  CarouselControlComponent,
+  CarouselIndicatorsComponent,
+  CarouselInnerComponent,
+  CarouselItemComponent,
+  ThemeDirective,
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [
+    DatePipe,
+    RouterModule,
+    ThemeDirective,
+    CarouselComponent,
+    CarouselIndicatorsComponent,
+    CarouselInnerComponent,
+    CarouselItemComponent,
+    CarouselControlComponent,
+  ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css',
 })
 export class ProjectComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  projectId!: any;
+  currentIndex = signal(0);
+  project!: ProjectInterface;
+  projects!: ProjectInterface[];
+  destroyRef: DestroyRef = inject(DestroyRef);
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store,
+    private router: Router
+  ) {}
   ngOnInit() {
-    console.log('data', this.route.params)
-    this.route.params.pipe(map( ({id}) => id), take(1)).subscribe(id => console.log('ID:', id))
+    forkJoin({
+      params: this.route.params.pipe(take(1)),
+      projects: this.store.select(selectProject).pipe(take(1)),
+    })
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(({ params: { id }, projects }) => {
+        this.projects = projects;
+        const idx = projects.findIndex((p) => p.id === Number(id));
+        if (idx !== -1) {
+          const project = projects[idx];
+          if (project) {
+            this.project = project;
+            this.currentIndex.set(idx);
+          }
+        }
+      });
+  }
+
+  onPrev() {
+    const idx = this.currentIndex();
+    const prev =
+      idx !== 0 ? (idx - 1) % this.projects.length : this.projects.length - 1;
+    this.currentIndex.set(prev);
+    this.project = this.projects[prev];
+    this.router.navigate(['../', this.projects[prev].id], {
+      relativeTo: this.route,
+    });
+  }
+  onNext() {
+    const idx = this.currentIndex();
+    const next = (idx + 1) % this.projects.length;
+    this.currentIndex.set(next);
+    this.project = this.projects[next];
+    this.router.navigate(['../', this.projects[next].id], {
+      relativeTo: this.route,
+    });
   }
 }
